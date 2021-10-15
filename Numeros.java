4cebc55ac7d76afee53a72c04ec48c80159b1b92
IgorMartins95 <90012950+IgorMartins95@users.noreply.github.com>
2021-10-15 00:08:59 -0300

    Add files via upload

---
+++ Numeros.java
@@ -0,0 +1,17 @@
+
+public class Numeros {
+
+	public static void main(String[] args) {
+
+		int i = 0;
+        int numero = 0;
+
+        while (i < 10)
+        {
+            numero = i;
+            i++;
+            System.out.println(numero);
+        }  
+	}
+
+}
